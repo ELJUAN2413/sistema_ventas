b105f7d0165faa067c14124fa8f7b0f81c8846e4
ELJUAN2413 <juancontrera02y@gmail.com>
2023-07-22 11:45:41 -0500

    modulo de autenticacion de usuarios

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\http\Controllers\CustomAuthController;
+
+
+route::get('dashboard', [CustomAuthController::class, 'dashboard']);
+route::get('login',[CustomAuthController::class, 'index'])->name('login');
+route::post('custom-login',[CustomAuthController::class, 'customlogin'])->name('login.custom');
+route::get('registration',[CustomAuthController::class, 'registration'])->name('register.user');
+route::post('custom-registration',[CustomAuthController::class, 'customregistration'])->name('register.custom');
+route::get('sigout',[CustomAuthController::class, 'sigout'])->name('sigout');
+
+
+
+
 
 /*
 |--------------------------------------------------------------------------
